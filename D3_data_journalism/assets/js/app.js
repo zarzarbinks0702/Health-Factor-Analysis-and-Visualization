a0362cf01635994a575b1a89c53aa4c36462dbe3
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-06-24 18:05:12 -0500

    update d3 version

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,24 @@
-// @TODO: YOUR CODE HERE!
+//set static svg/chart variables
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+//svg wrapper and append to index.html
+var svg = d3
+  .select('.chart')
+  .append('svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight);
+
+//append svg to chart group
+var chartGroup = svg.append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
