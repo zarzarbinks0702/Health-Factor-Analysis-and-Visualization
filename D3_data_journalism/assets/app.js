18c41a317db9591d1e93d895cf7b10109c7731e6
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-06-25 15:06:51 -0500

    added axes labels

--- D3_data_journalism/assets/app.js
+++ D3_data_journalism/assets/app.js
@@ -7,9 +7,9 @@ var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
-  top: 20,
+  top: 30,
   right: 40,
-  bottom: 80,
+  bottom: 100,
   left: 100
 };
 
@@ -17,8 +17,7 @@ var chartWidth = svgWidth - margin.left - margin.right;
 var chartHeight = svgHeight - margin.top - margin.bottom;
 
 //svg wrapper and append to index.html
-var svg = d3
-  .select('#scatter')
+var svg = d3.select('#scatter')
   .append('svg')
   .attr('width', svgWidth)
   .attr('height', svgHeight);
@@ -31,7 +30,7 @@ var chartGroup = svg.append('g')
 //function for converting the relevant lines of the csv to integers
 function rowUpdate(row) {
   row.income = +row.income;
-  row.obesity = +row.obesity;
+  row.smokes = +row.smokes;
   return row;
 }
 /***********************************************************************/
@@ -47,13 +46,13 @@ function createScatter(data) {
     .domain([0, d3.max(data, (d) => d.income)])
     .range([chartHeight, 0]);
 
-  //create x axis scale (obesity)
-  let xObesityScale = d3.scaleLinear()
-    .domain([0, d3.max(data, (d) => d.obesity)])
+  //create x axis scale (smokes)
+  let xSmokesScale = d3.scaleLinear()
+    .domain([0, d3.max(data, (d) => d.smokes)])
     .range([0, chartWidth]);
 
   //add the axis to the chart
-  let xAxis = d3.axisBottom(xObesityScale);
+  let xAxis = d3.axisBottom(xSmokesScale);
   let yAxis = d3.axisLeft(yIncomeScale);
   chartGroup.append("g").call(yAxis);
   chartGroup.append("g").call(xAxis).attr("transform", `translate(0, ${chartHeight})`);
@@ -63,10 +62,24 @@ function createScatter(data) {
       .data(data)
       .enter()
       .append("circle")
-      .attr("cx", d => xObesityScale(d.obesity))
+      .attr("cx", d => xSmokesScale(d.smokes))
       .attr("cy", d => yIncomeScale(d.income))
       .attr("r", "15")
       .attr("fill", "blue")
       .attr("opacity", '0.5');
+
+  //add state abbreveations to the circles
+  //add axes labels to chart
+    chartGroup.append("text")
+    .attr("class", "axisText")
+    .text("Median Income (USD)")
+    .attr("transform", ` translate(-55, ${chartHeight / 1.4}) rotate(-90) scale(1.1)`);
+
+  chartGroup.append("text")
+    .attr("x", chartWidth / 2.5)
+    .attr("y", chartHeight + 10)
+    .attr("class", "axisText")
+    .text("Smokers (%)")
+    .attr('transform', 'scale(1.1)');
 }
 /***********************************************************************/
